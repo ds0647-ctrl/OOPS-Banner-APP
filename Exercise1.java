4fb0fa4530c783c345d848cde12957fac5aad15f
ds0647-ctrl <ds0647@srmist.edu.in>
2026-02-24 09:59:19 +0530

    Add Exercise1.java

---
+++ Exercise1.java
@@ -0,0 +1,18 @@
+public class Exercise1 {
+    public static void main(String[] args) {
+        int number = 7;
+
+        // If-else control flow
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+
+        // Loop control flow
+        System.out.println("Counting from 1 to 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
